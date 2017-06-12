5354f3f9c599986048595d4f8a17939a20be6658
JoohyeongSong <jhsong206@naver.com>
2017-06-12 21:55:28 +0900

    Update EV3_mazeSolve.java

--- EV3_mazeSolve.java
+++ EV3_mazeSolve.java
@@ -40,7 +40,7 @@ public class EV3_mazeSolve
 
 // Move robot and detect wall
 class SenseAndMove implements Behavior {
-	final int OneBlock = 540;
+	final int OneBlock = 400;
 	static boolean mutex = false;
 	final int RED = 0;
 	final int BLACK = 1;
@@ -55,62 +55,112 @@ class SenseAndMove implements Behavior {
 	static boolean direction_flag = true;
 	static Integer rotate_size = 0;
 	
-	public void Process_Red_Wall() {
+	static Integer forward_angle;
+	static Integer previous_angle = 0;
+	public static int[] path = new int[1000];
+	//public static Byte[] path = new Byte[1000];
+	public static int path_index = 1;
+	
+	static boolean write_mutex = false;
+	int SendData[] = {10, 1, 2, 3, 2, 3, 3, 3, 2, 1, 1};
+	//Byte SendDataByte[] = {10, 1, 2, 3, 2, 3, 3, 3, 2, 1, 1};
+	
+	
+	public void Process_Blue_Wall() {
 		LCD.clearDisplay();
-		LCD.drawString("RED", 0, 1);
+		LCD.drawString("BLUE", 0, 1);
 		EV3_mazeSolve.move.rightTurn(180);
+		previous_angle = forward_angle;
+		path[path_index++] = 3;
 		Delay.msDelay(2000);
+		/*
 		EV3_mazeSolve.move.forwardOneblock(OneBlock);
+		previous_angle = forward_angle;
+		path[path_index++] = 1;
 		Delay.msDelay(4000);
+		*/
 	}
 	
 	public void Process_Black_Wall() {
 		LCD.clearDisplay();
 		LCD.drawString("BLACK", 0, 1);
 		
-		// Wait for data to drain
-		LCD.clear();
-		LCD.drawString("closing",0,3);
-		LCD.refresh();
-
+		/*
+		for(int i = 0; i < path_index; i++){
+			LCD.drawString("" + path[i], i, 6);					
+		}
+		*/
+		
+		EV3_mazeSolve.move.stop();
+		
 		try {
-			EV3_BluetoothConnect.btc.close();
+			if (!write_mutex) {
+				write_mutex = true;
+				path[0] = path_index-1;
+				EV3_BluetoothConnect.DeliverPath(path);
+				write_mutex = false;
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-
-		LCD.clear();
+		Delay.msDelay(5000);
+		
+		LCD.clearDisplay();
+		LCD.drawString("Ending Program", 0, 2);
+		
+		for (Integer count = 5; count > 0; count--) {
+			LCD.drawString(count.toString(), 0, 3);
+			Delay.msDelay(1000);
+		}
+		
 		System.exit(-1);
 	}
 	
-	public void Process_Blue_Wall() {
+	public void Process_Red_Wall() {
 		LCD.clearDisplay();
-		LCD.drawString("BLUE", 0, 1);
+		LCD.drawString("RED", 0, 1);
 		EV3_mazeSolve.move.leftTurn(180);
+		previous_angle = forward_angle;
+		path[path_index++] = 2;
+	
 		Delay.msDelay(2000);
+		
 		EV3_mazeSolve.move.forwardOneblock(OneBlock);
-		Delay.msDelay(4000);
+		previous_angle = forward_angle;
+		path[path_index++] = 1;
+		
+		Delay.msDelay(2000);
 	}
 	
 	public void Process_Yellow_Wall() {
 		LCD.clearDisplay();
 		LCD.drawString("YELLOW", 0, 1);
 		EV3_mazeSolve.move.leftTurn(180);
+		previous_angle = forward_angle;
+		path[path_index++] = 2;
 		Delay.msDelay(2000);
 		EV3_mazeSolve.move.forwardOneblock(OneBlock);
+		previous_angle = forward_angle;
+		path[path_index++] = 1;
 		Delay.msDelay(4000);
 		EV3_mazeSolve.move.rightTurn(180);
+		previous_angle = forward_angle;
+		path[path_index++] = 3;
 		Delay.msDelay(2000);
+		/*
 		EV3_mazeSolve.move.forwardOneblock(OneBlock);
+		previous_angle = forward_angle;
+		path[path_index++] = 1;
 		Delay.msDelay(4000);
+		*/
 	}
 	
 	@Override
 	public boolean takeControl() {
 		int color = EV3_mazeSolve.color.getColor();
 		float light = EV3_mazeSolve.light.getLight();
+		
 		
 		if (EV3_BluetoothConnect.OnOff_Flag == false) {
 			EV3_mazeSolve.move.stop();
@@ -123,20 +173,44 @@ class SenseAndMove implements Behavior {
 			if (color == RED){
 				Process_Red_Wall();
 			}
+			/*
 			else if (color == BLACK){
 				Process_Black_Wall();
 			}
+			*/
 			else if (color == BLUE){
 				Process_Blue_Wall();
 			}
 			else if (color == YELLOW){
 				Process_Yellow_Wall();
+				
 			}
-			else if (color == GREEN) {
+			else if (color == BLACK) {
+				for(int i = 0; i < path_index; i++){
+					LCD.drawString("" + path[i], i, 6);					
+				}
+				
+				EV3_mazeSolve.move.stop();
+				try {
+					for (int i = 0; i < 20; i++) {
+					if (!write_mutex) {
+						write_mutex = true;
+						path[0] = path_index-1;
+						EV3_BluetoothConnect.DeliverPath(path);
+						write_mutex = false;
+					}
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				EV3_mazeSolve.move.stop();
+				EV3_BluetoothConnect.End_flag = true;
 				System.exit(-1);
 			}
 			mutex = false;
-			return false;
+			
 		}
 		
 		// Sense White
@@ -155,13 +229,28 @@ class SenseAndMove implements Behavior {
 	public void suppress() {
 	}
 	
+
 	@Override
 	public void action() {
 		// if Black, Go through
 		if (white_flag == false) {
-			EV3_mazeSolve.move.forward();
+			
+			
+			forward_angle = EV3_mazeSolve.move.forward();
+			
+			
+			if(forward_angle - previous_angle > OneBlock){
+				LCD.drawString("" + path_index, 0, 5);
+				previous_angle = forward_angle;
+				path[path_index++] = 1;
+				
+			}
+			Integer tmp = forward_angle - previous_angle;
+			
 			Float v = EV3_mazeSolve.light.getLight();
+			//LCD.clearDisplay();
 			LCD.drawString(v.toString(), 0, 4);
+			LCD.drawString(tmp.toString(), 0, 7);
 			rotate_size = 20;
 		}
 		// if White, Find path
